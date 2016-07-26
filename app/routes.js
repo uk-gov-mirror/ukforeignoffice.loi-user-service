e813cd5ac7c4345e7fa021fb991cb7ccd3945758
unknown <shieldsj@KARACHI.informed.co.uk>
2016-07-26 16:20:25 +0100

    Change text of console info message for invalid email/password combination.

--- app/routes.js
+++ app/routes.js
@@ -63,7 +63,11 @@ module.exports = function(express,envVariables) {
             error_subitem = 'Check your email and password and try again';
         }
         if (error.length>0){
-            console.info('Failed Sign In Attempt: '+ error);
+            var info_text = error;
+            if (info_text == 'There was a problem signing in') {
+                info_text = 'The specified email and password combination does not exist';
+            }
+            console.info('Failed Sign In Attempt: '+ info_text);
         }
         //render page and pass in flash data if any exists
         var back_link = '/api/user/usercheck';
