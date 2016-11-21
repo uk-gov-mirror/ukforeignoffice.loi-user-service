f190e3285413dab8feafa3304055243d258f60dd
simonmoore100 <s.moore@kainos.com>
2016-11-21 09:21:03 +0000

    Tidy up env file

--- config/environment.js
+++ config/environment.js
@@ -16,8 +16,7 @@ var notificationserviceurl = JSON.parse(env.NOTIFICATIONSERVICEURL);
 var passwordsettings = JSON.parse(env.PASSWORDSETTINGS);
 var postcodelookupoptions = JSON.parse(env.POSTCODELOOKUPOPTIONS);
 var live_variables = JSON.parse(env.LIVEVARIABLES);
-var daysBeforeAccountIsDeleted = JSON.parse(env.DAYSBEFOREACCOUNTISDELETED);
-var jobSchedule = env.JOBSCHEDULE;
+var userAccountSettings = JSON.parse(env.USERACCOUNTSETTINGS);
 var pgPassword = env.PGPASSWORD;
 var Sequelize = require('sequelize');
 
@@ -33,8 +32,7 @@ var config = {
     "password_settings":passwordsettings,
     "postcodeLookUpApiOptions":postcodelookupoptions,
     "pgpassword": pgPassword,
-    "daysBeforeAccountIsDeleted": daysBeforeAccountIsDeleted,
-    "jobSchedule": jobSchedule,
+    "userAccountSettings": userAccountSettings,
     "live_variables":live_variables
 };
 
--- config/jobs.js
+++ config/jobs.js
@@ -9,34 +9,25 @@ var jobs ={
     accountExpiryCheck: function(){
     console.log("RUNNING ACCOUNT EXPIRY CHECK JOB");
         var now = new Date(),
-        aWeekLater = new Date(now);
+        gracePeriod = new Date(now);
 
-        aWeekLater.setDate(now.getDate()+7);
-
+        gracePeriod.setDate(now.getDate()+envVariables.userAccountSettings.gracePeriod);
         Model.User.findAll().then(function(users){
                 for(var u=0; u<users.length; u++){
                     var user = users[u],
                         expired = user.accountExpiry < now,
-                        expiringThisWeek = user.accountExpiry < aWeekLater,
+                        expiringSoon = user.accountExpiry < gracePeriod,
                         warningSent = user.warningSent,
-                        expiryConfirmationSent = user.expiryConfirmationSent,
-                        diffTime = now.getTime() - user.accountExpiry.getTime(),
-                        diffDays = Math.round(Math.abs(diffTime/(1000*60*60*24))),
-                        readyToDelete = diffDays > envVariables.daysBeforeAccountIsDeleted;
-
-                    if(!expired && expiringThisWeek && !warningSent){
+                        expiryConfirmationSent = user.expiryConfirmationSent;
+                    if(!expired && expiringSoon && !warningSent){
                         console.log('SEND WARNING EMAIL');
                         Model.User.update({warningSent:true}, {where:{email : user.email}});
                         emailService.expiryWarning(user.email);
                     }
                     else if(expired && !expiryConfirmationSent){
-                        console.log('SEND EXPIRY EMAIL');
+                        console.log('SEND EXPIRY EMAIL AND DELETE ACCOUNT');
                         Model.User.update({expiryConfirmationSent:true}, {where:{email : user.email}});
                         emailService.expiryConfirmation(user.email);
-                        
-                    }
-                    else if(expired && expiryConfirmationSent && readyToDelete){
-                        console.log('DELETE USER ACCOUNT');
                         Model.AccountDetails.destroy({where:{user_id:user.id}});
                         Model.SavedAddress.destroy({where:{user_id:user.id}});
                         Model.User.destroy({where:{email:user.email}});
