2ac029fb5fa452132b0e01e2db03462cb5fcc797
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2021-04-13 12:13:03 +0100

    Randomise hour/min for expiry job

--- server.js
+++ server.js
@@ -91,9 +91,14 @@ app.use(bodyParser.urlencoded({
 
 var schedule = require('node-schedule');
 
+//Expiry Job completed every day.
 var jobs = require('./config/jobs.js');
-//Expiry Job completed at 6:00AM every day.
-var ExpiryJob = schedule.scheduleJob(environmentVariables.userAccountSettings.jobSchedule, function(){jobs.accountExpiryCheck()});
+// As there are 2 instances running, we need a random time, or two emails will be sent
+// for accounts nearing expiration. (Flag will be set by time of 2nd job execution to stop duplicate)
+var randomMin = Math.floor(Math.random() * 60);
+var randomHour = Math.floor(Math.random() * 8);
+var jobScheduleRandom = "0 " + randomMin + " " + randomHour + " * * *";
+var ExpiryJob = schedule.scheduleJob(jobScheduleRandom, function(){jobs.accountExpiryCheck()});
 
 
 passportConfig(app, passport);
